806fcf7c940088ce586dcfabe1158ebfcb4f13ed
Ji Kim <jkim55@gmail.com>
2016-07-30 18:05:29 -0600

    can render countryjson per search box input

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,20 @@
-$( function() {
+$(function() {
   var mainURL="https://galvanize-cors-proxy.herokuapp.com/https://travelbriefing.org/countries.json"
   var countryNames=[]
 
-  $.get(mainURL).then(function (countryArr) {
+  $.get(mainURL)
+  .then((countryArr)=>{
     for (var countryIndex in countryArr)
     countryNames.push(countryArr[countryIndex].name)})
 
-  $( "#countrySearch" ).autocomplete({
+  $("#searchBox").autocomplete({
     source: countryNames
   });
 });
+
+$("#submit").click(()=>{
+  let countrySelected = $('#searchBox').val();
+  var countryURL = "https://travelbriefing.org/"+countrySelected+"?format=json"
+  // $.get(countryURL)
+
+})
