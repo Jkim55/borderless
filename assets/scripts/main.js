c70bbd024d7ad292c94aa25dc23df2f11d015118
Ji Kim <jkim55@gmail.com>
2016-08-01 02:23:09 -0600

    more drilling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,11 +25,17 @@ function getCountryJSON(){
     console.log("full name: ", data.names.full)
     // flag: "http://www.geonames.org/flags/x/" + (data["names"]["iso2"]) + ".gif"   // ie http://www.geonames.org/flags/x/jm.gif
     console.log("Array of languages: ", data.language); //iterate thru the array of objs; keys: language & official
+    console.log(data.maps.lat, data.maps.long, data.maps.zoom) //iterate thru obj; keys: lat, long, zoom
     console.log("Country code: ", data.telephone.calling_code);
+    console.log("Electrical: ", data.electricity) //iterate thru obj; keys: voltage, frequency, plugs
     console.log("Police: ", data.telephone.police);
-    console.log("Water Drinkability Advisement: ", data.water.short);
+
     console.log("Vaccinations required: ", data.vaccinations); //iterate thru array of objs; keys: message & name
     console.log("Travel Advisories: ", data.advise); // Object of objs with key (country ISO2) and values of an obj that has keys of advise & URL (will want to correlate with ISO- http://country.io/names.json)
+    console.log("Water Drinkability Advisement: ", data.water.short);
+    console.log("Currency: ", data.currency.name, data.currency.symbol, data.currency.rate, data.currency.compare[151].name, data.currency.compare[151].rate) //iterate thru obj; keys: name, symbol, rate
+    // ADD: Visa information - either create one yourself or continue looking for an API
+
   })
 
 }
