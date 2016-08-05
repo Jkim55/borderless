813335bc2994a42443ec1316c3b1ebe20539a080
Ji Kim <jkim55@gmail.com>
2016-08-05 16:59:56 -0600

    mid sec3- nyt works, guardian is next

--- assets/scripts/section3.js
+++ assets/scripts/section3.js
@@ -10,35 +10,23 @@
 function buildSection3() {
   nytURL("economy")
   // guardianURL("economy", "politics")
-  // fetchStories()
+  // fetchData()
 }
-economy / politics
-travel
 
-function nytURL(topic) {
+function nytURL(topic) {  // topics to cover (1) economy & politics (2) travel, arts & culture
   let requestedEndDate = setEndDate()
   let nytAPIURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+ nyTimesKey + "&q=" + countryName + "," + topic + "&begin_date=" + requestedEndDate + "&sort=newest"
 // return nytAPIURL
   console.log(nytAPIURL)
 }
 
-// var exampleOfGuardianAPIURL='http://content.guardianapis.com/search?q=kenya%20AND%20(economy%20OR%20politics)&tag=type/article&from-date=2016-07-01&api-key=c9d22c79-4372-4306-97ff-09edf0781da6'
-
-function guardianURL(topic1, topic2) {
+function guardianURL(topic1, topic2) { // topics to cover (1) economy & politics (2) travel, arts & culture 
   let requestedEndDate = setEndDate()
   let guardianAPIURL='http://content.guardianapis.com/search?q='+ countryName + '%20AND%20('+ topic1 + '%20OR%20' + topic2 + ')&tag=type/article&from-date=' + requestedEndDate + '&api-key=' + guardianKey
   // return guardianAPIURL
   console.log(guardianAPIURL)
-
 }
-
-function fetchStories(newsURL) {
-  $.get(newsURL)
-  .then((data)=>{
-    console.log(data)
-    console.log(JSON.stringify(data))
-  })
-}
+// var exampleOfGuardianAPIURL='http://content.guardianapis.com/search?q=kenya%20AND%20(economy%20OR%20politics)&tag=type/article&from-date=2016-07-01&api-key=c9d22c79-4372-4306-97ff-09edf0781da6'
 
 function setEndDate() {
   let endDate = new Date()
@@ -47,13 +35,24 @@ function setEndDate() {
   return endDate
 }
 
+function fetchData(newsURL) {
+  $.get(newsURL)
+}
+function parseData(){
+  .then((data)=>{
+    console.log(data)
+    console.log(JSON.stringify(data))
+  })
+}
+
+
 
 // var exampleOfGuardianAPIURL='http://content.guardianapis.com/search?q=kenya%20AND%20(economy%20OR%20politics)&tag=type/article&from-date=2016-07-01&api-key=c9d22c79-4372-4306-97ff-09edf0781da6'
 
 // // NYTIMES Data - iterate thru each article returned
+
 // let articlesArr = data["response"]["docs"]  //arr of obj
 // var newsImages = "https://static01.nyt.com/" + photo  // photo of article
-
 // let headline = data["response"]["docs"][0]["headline"]["main"]
 // let url = data["response"]["docs"][0]["web_url"]
 // let snippet = data["response"]["docs"][0]["snippet"]
