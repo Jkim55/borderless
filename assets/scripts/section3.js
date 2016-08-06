f3b28d7da79df294427255e8d7a7c60d5aaadda9
Ji Kim <jkim55@gmail.com>
2016-08-06 13:18:05 -0600

    cleaned up

--- assets/scripts/section3.js
+++ assets/scripts/section3.js
@@ -1,8 +1,4 @@
 //***********************   DO NOT PUBLISH KEY!!!!!   ***********************
-// either bing, npr or guardian since tem b*tches at BBC never got back to you with a KEY
-// read documentation and see which of the three you want to work with
-// stretch goal - use all 3 or 2
-
 // GLOBAL VARIABLES
 
 
--- assets/scripts/section4.js
+++ assets/scripts/section4.js
@@ -14,7 +14,3 @@
 // either bing, npr or guardian since tem b*tches at BBC never got back to you with a KEY
 // read documentation and see which of the three you want to work with
 // stretch goal - use all 3 or 2
-
-
-var exampleOfGuardianAPIURL='http://content.guardianapis.com/search?q=kenya%20AND%20(economy%20OR%20politics)&tag=type/article&from-date=2016-07-01&api-key=c9d22c79-4372-4306-97ff-09edf0781da6'
-var exampleOfGuardianAPIURL='http://content.guardianapis.com/search?q=' + countryName +'%20AND%20('+ topic +')&tag=type/article&from-date=2016-07-01&api-key=c9d22c79-4372-4306-97ff-09edf0781da6'
